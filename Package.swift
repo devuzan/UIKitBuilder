5f6925af05eb59a259725ce159fb5a129c07eb22
Yusuf U <devuzan@gmail.com>
2022-08-23 12:54:43 +0300

    MPS added.

--- Package.swift
+++ Package.swift
@@ -13,16 +13,17 @@ let package = Package(
             targets: ["UIKitBuilder"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-
+      .package(
+        url: "https://github.com/devuzan/MorePowerfulSwift",
+        from: "0.0.1"
+      )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UIKitBuilder",
-            dependencies: []),
+            dependencies: ["MorePowerfulSwift"]),
         .testTarget(
             name: "UIKitBuilderTests",
             dependencies: ["UIKitBuilder"]),
---
+++ Sources/UIKitBuilder/UIKitBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yusuf Uzan on 23.08.2022.
+//
+
+import MorePowerfulSwift
