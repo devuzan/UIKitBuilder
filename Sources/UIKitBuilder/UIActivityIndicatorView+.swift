fc094407633855232ef54ecf8a1052edccc3d5c6
Yusuf U <devuzan@gmail.com>
2022-08-27 12:04:31 +0300

    Initial commit.

--- Sources/UIKitBuilder/UIActivityIndicatorView+.swift
+++ Sources/UIKitBuilder/UIActivityIndicatorView+.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 public extension UIActivityIndicatorView {
+  
   @discardableResult func style(_ style: UIActivityIndicatorView.Style) -> Self {
     self.style = style
     return self
   }
+  
   @discardableResult func tintColor(_ tintColor: UIColor) -> Self {
     self.tintColor = tintColor
     return self
   }
+  
   @discardableResult func hidesWhenStopped(_ hidesWhenStopped: Bool) -> Self {
     self.hidesWhenStopped = hidesWhenStopped
     return self
   }
+  
+  @discardableResult func color(_ color: UIColor) -> Self {
+    self.color = color
+    return self
+  }
+
 }
