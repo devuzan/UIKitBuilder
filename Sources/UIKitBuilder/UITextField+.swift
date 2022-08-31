9708f5dfac2560249f4508808be3819bfb53f8d8
Yusuf U <devuzan@gmail.com>
2022-08-31 10:13:01 +0300

    Textfield shouldReturn added.

--- Sources/UIKitBuilder/UITextField+.swift
+++ Sources/UIKitBuilder/UITextField+.swift
@@ -9,17 +9,22 @@ import UIKit
 
 public extension UITextField {
   
-  @discardableResult func textColor(_ textColor: UIColor) -> UITextField {
+  @discardableResult func textColor(_ textColor: UIColor) -> Self {
     self.textColor = textColor
     return self
   }
   
-  @discardableResult func font(_ font: UIFont) -> UITextField {
+  @discardableResult func font(_ font: UIFont) -> Self {
     self.font = font
     return self
   }
   
-  @discardableResult func placeholder(_ placeholderFont: UIFont, text: String, textColor: UIColor?) -> UITextField {
+  @discardableResult func clearButtonMode(_ clearButtonMode: UITextField.ViewMode) -> Self {
+    self.clearButtonMode = clearButtonMode
+    return self
+  }
+
+  @discardableResult func placeholder(_ placeholderFont: UIFont, text: String, textColor: UIColor?) -> Self {
     if let color = textColor {
       let attributedString = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.font: font!, NSAttributedString.Key.foregroundColor: color])
       self.attributedPlaceholder = attributedString
@@ -27,7 +32,7 @@ public extension UITextField {
     return self
   }
   
-  @discardableResult func borderStyle(_ borderStyle: UITextField.BorderStyle) -> UITextField {
+  @discardableResult func borderStyle(_ borderStyle: UITextField.BorderStyle) -> Self {
     self.borderStyle = borderStyle
     return self
   }
@@ -37,32 +42,37 @@ public extension UITextField {
     return self
   }
   
-  @discardableResult func keyboardType(_ keyboardType: UIKeyboardType) -> UITextField {
+  @discardableResult func keyboardType(_ keyboardType: UIKeyboardType) -> Self {
     self.keyboardType = keyboardType
     return self
   }
   
-  @discardableResult func placeholder(_ placeholder: String) -> UITextField {
+  @discardableResult func placeholder(_ placeholder: String) -> Self {
     self.placeholder = text
     return self
   }
   
-  @discardableResult func autocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> UITextField {
+  @discardableResult func autocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> Self {
     self.autocorrectionType = autocorrectionType
     return self
   }
   
-  @discardableResult func userInteractionEnabled(_ isUserInteractionEnabled: Bool = true) -> UITextField {
+  @discardableResult func userInteractionEnabled(_ isUserInteractionEnabled: Bool = true) -> Self {
     self.isUserInteractionEnabled = isUserInteractionEnabled
     return self
   }
   
-  @discardableResult func rightViewText(_ text: String) -> UITextField {
+  @discardableResult func rightViewText(_ text: String) -> Self {
     let view = UILabel(frame: CGRect(x: 0, y: 0, width: 20, height: 10))
       .text(text)
       .textAlignment(.center)
     rightViewMode = .always
     rightView = view
+    return self
+  }
+  
+  @discardableResult func delegate(_ delegate: AnyObject) -> Self {
+    self.delegate = delegate as? UITextFieldDelegate
     return self
   }
   
